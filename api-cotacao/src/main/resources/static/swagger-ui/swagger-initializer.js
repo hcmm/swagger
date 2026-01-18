90f8338f51db9ceb74513fb87ccfd2402173aca6
Henrique Canto <henrique_canto3@hotmail.com>
2026-01-18 13:27:31 -0300

    feat: ajustes paths swagger

--- api-cotacao/src/main/resources/static/swagger-ui/swagger-initializer.js
+++ api-cotacao/src/main/resources/static/swagger-ui/swagger-initializer.js
@@ -2,7 +2,7 @@ window.onload = function() {
   //<editor-fold desc="Changeable Configuration Block">
 
   window.ui = SwaggerUIBundle({
-    url: "/openapi/swagger.yaml",
+    url: "/swagger-ui/swagger.yaml",
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
